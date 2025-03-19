5c71c312a9d6c857b9d00e0eb8e5265701a706c9
sebasBetancourt <s3basbetan@gmail.com>
2025-03-19 08:51:42 -0500

    style: :art: Add Images of window

--- js/game.js
+++ js/game.js
@@ -3,7 +3,7 @@ export const configuration = () => {
         type: Phaser.AUTO,
         width: 256,
         height: 244,
-        backgroundColor: '#fff',
+        backgroundColor: '#049cd8',
         parent: 'game',
         scene: {
             preload,
@@ -15,12 +15,37 @@ export const configuration = () => {
     new Phaser.Game(config)
 
     function preload () {
-        console.log('preload');
+        this.load.image(
+            'cloud1',
+            './assets/scenery/overworld/cloud1.png'
+        )
+
+
+        this.load.image(
+            'floorbricks',
+            './assets/scenery/overworld/floorbricks.png'
+        )
+
+        this.load.spritesheet(
+            'mario',
+            './assets/entities/mario.png',
+            { frameWidth: 18, frameHeight: 16 }
+        )
     }
+
+    
+
     function create () {
-        console.log('create');
+        this.add.image(100, 50, 'cloud1')
+            .setOrigin(0, 0)
+            .setScale(0.15)
+
+        this.add.tileSprite(0, config.height, config.width, 32, 'floorbricks')
+
+        this.add.sprite(50, 200, 'mario')
+            .setOrigin(0, 1)
     }
     function update () {
-
+        
     }
 };
